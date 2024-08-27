f9662efe003159d39550063dab19210aaeb7f514
abdullahsn10 <1201952@student.birzeit.edu>
2024-08-27 22:48:30 +0300

    Add migrations using Alembic

---
+++ src/models/inventory_item.py
@@ -0,0 +1,27 @@
+from src.settings.database import Base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DOUBLE_PRECISION,
+    Boolean,
+    DATE,
+)
+
+
+class InventoryItem(Base):
+    """
+    SQLAlchemy model for inventory items table
+    """
+
+    __tablename__ = "inventory_item"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    price = Column(DOUBLE_PRECISION, nullable=False)
+    expire_date = Column(DATE)
+    prod_date = Column(DATE)
+    available_quantity = Column(Integer, nullable=False)
+    deleted = Column(Boolean, default=False)
+    # relationship with Coffee Shop
+    coffee_shop_id = Column(Integer, nullable=False, index=True)
