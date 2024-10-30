9aac64bddd04280ee21491adb7d5800a21e6d640
Abdullah S. Naser <121259956+abdullahsn10@users.noreply.github.com>
2024-10-30 15:25:37 +0200

    K8s deployment (#5)
    * Update settings
    
    * Add Dockerfile and init script
    
    * Update the dockerfile
    
    * Add projects configs as env var
    
    ---------
    
    Co-authored-by: Abdullah Naser <Abdullah.Naser@exalt.ps>

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from src.routers import inventory_item
+from src.settings.settings import OPENAPI_URL, ROOT_PATH
 
-app = FastAPI()
+app = FastAPI(
+    openapi_url=OPENAPI_URL,
+    root_path=ROOT_PATH,
+)
 
 # register routes
 app.include_router(inventory_item.router)
